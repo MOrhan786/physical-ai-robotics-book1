fa14c0f338053c26376fcc0c01cd3ac538b98b40
Fatima <fatima787asif@gmail.com>
2025-12-12 18:48:58 +0500

    vercel settin

--- book/docusaurus.config.ts
+++ book/docusaurus.config.ts
@@ -15,7 +15,11 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  // add my line
+  url: 'https://physical-ai-robotics-book1-ifiy.vercel.app',
+  // url: 'https://your-docusaurus-site.example.com', this line i was commit
+
+
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
